3394514348b087f75e1fb8d2d3c2b4eaeee1dc5c
manuel82ottaviani <manuel82.ottaviani@gmail.com>
2025-10-22 16:47:38 +0200

    Delete fattore_rischio.js

--- fattore_rischio.js
+++
@@ -1,7 +0,0 @@
-function calcola(){
-  var p = parseFloat(document.getElementById('prob').value);
-  var f = parseFloat(document.getElementById('freq').value);
-  var g = parseFloat(document.getElementById('grav').value);
-  var r = p * f * g;
-  document.getElementById('risultato').innerText = "Fattore di rischio: " + r;
-}
